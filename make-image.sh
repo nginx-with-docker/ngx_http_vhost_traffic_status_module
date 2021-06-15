7bc2412b4c8592ba728687240bd5e31104f608fc
soulteary <soulteary@gmail.com>
2021-06-15 18:10:33 +0800

    split versions

--- make-image.sh
+++ make-image.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
+PROJECT_NAME="vts"
+RELEASE_DIR="./docker";
 REPO_NAME="soulteary/prebuilt-nginx-modules"
-tag="vts";
 
-set -a
-    . "docker/.env"
-set +a
+for subVer in $RELEASE_DIR/*; do
 
+    set -a
+        . "$subVer/.env"
+    set +a
 
-if [ -f "docker/Dockerfile.alpine" ]; then
-    BUILD_NAME="$REPO_NAME:$tag-$NGINX_VERSION-alpine"
-    if [[ "$(docker images -q $BUILD_NAME 2> /dev/null)" == "" ]]; then
-        echo "Build: $BUILD_NAME";
-        BUILD_ARGS=$(tr '\n' ';' < "docker/.env" | sed 's/;$/\n/' | sed 's/^/ --build-arg /' | sed 's/;/ --build-arg /g')
-        docker build $BUILD_ARGS --tag $BUILD_NAME -f docker/Dockerfile.alpine .
+    REPO_TAG="ngx-$NGINX_VERSION-$PROJECT_NAME-$MODULE_VERSION";
+    BUILD_ARGS=$(tr '\n' ';' < "$subVer/.env" | sed 's/;$/\n/' | sed 's/^/ --build-arg /' | sed 's/;/ --build-arg /g')
+
+    if [ -f "$subVer/Dockerfile.alpine" ]; then
+        BUILD_NAME="$REPO_NAME:$REPO_TAG-alpine"
+        if [[ "$(docker images -q $BUILD_NAME 2> /dev/null)" == "" ]]; then
+            echo "Build: $BUILD_NAME";
+            docker build $BUILD_ARGS --tag $BUILD_NAME -f $subVer/Dockerfile.alpine .
+        fi
     fi
-fi
 
-if [ -f "docker/Dockerfile.debian" ]; then
-    BUILD_NAME="$REPO_NAME:$tag-$NGINX_VERSION"
-    if [[ "$(docker images -q $BUILD_NAME 2> /dev/null)" == "" ]]; then
-        echo "Build: $BUILD_NAME";
-        BUILD_ARGS=$(tr '\n' ';' < "docker/.env" | sed 's/;$/\n/' | sed 's/^/ --build-arg /' | sed 's/;/ --build-arg /g')
-        docker build $BUILD_ARGS --tag $BUILD_NAME -f docker/Dockerfile.debian .
+    if [ -f "$subVer/Dockerfile.debian" ]; then
+        BUILD_NAME="$REPO_NAME:$REPO_TAG"
+        if [[ "$(docker images -q $BUILD_NAME 2> /dev/null)" == "" ]]; then
+            echo "Build: $BUILD_NAME";
+            docker build $BUILD_ARGS --tag $BUILD_NAME -f $subVer/Dockerfile.debian .
+        fi
     fi
-fi
+
+done
--- push-image.sh
+++ push-image.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
-REPO_NAME="soulteary/prebuilt-nginx-modules";
-tag="vts";
+PROJECT_NAME="vts"
+REPO_NAME="soulteary/prebuilt-nginx-modules"
+RELEASE_DIR="./docker";
 
-set -a
-    . "docker/.env"
-set +a
+for subVer in $RELEASE_DIR/*; do
+    set -a
+        . "$subVer/.env"
+    set +a
 
+    REPO_TAG="ngx-$NGINX_VERSION-$PROJECT_NAME-$MODULE_VERSION";
 
-if [ -f "docker/Dockerfile.alpine" ]; then
-    BUILD_NAME="$REPO_NAME:$tag-$NGINX_VERSION-alpine"
-    if [[ "$(docker images -q $BUILD_NAME 2> /dev/null)" != "" ]]; then
-        echo "Push: $BUILD_NAME";
-        docker push $BUILD_NAME;
+    if [ -f "$subVer/Dockerfile.alpine" ]; then
+        BUILD_NAME="$REPO_NAME:$REPO_TAG-alpine"
+        if [[ "$(docker images -q $BUILD_NAME 2> /dev/null)" != "" ]]; then
+            echo "Push: $BUILD_NAME";
+            docker push $BUILD_NAME;
+        fi
     fi
-fi
 
-if [ -f "docker/Dockerfile.debian" ]; then
-    BUILD_NAME="$REPO_NAME:$tag-$NGINX_VERSION"
-    if [[ "$(docker images -q $BUILD_NAME 2> /dev/null)" != "" ]]; then
-        echo "Push: $BUILD_NAME";
-        docker push $BUILD_NAME;
+    if [ -f "$subVer/Dockerfile.debian" ]; then
+        BUILD_NAME="$REPO_NAME:$REPO_TAG"
+        if [[ "$(docker images -q $BUILD_NAME 2> /dev/null)" != "" ]]; then
+            echo "Push: $BUILD_NAME";
+            docker push $BUILD_NAME;
+        fi
     fi
-fi
 
+done
